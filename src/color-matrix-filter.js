044c2599f6db5ac9670cbeeaf052a32037d30eda
Steven <trthtai92@gmail.com>
2019-11-05 22:45:11 +0700

    - add get image data function for react native.

--- src/color-matrix-filter.js
+++ src/color-matrix-filter.js
@@ -1,72 +1,94 @@
-import React from 'react'
-import { NativeFilter } from './native-filter'
-import filters from 'rn-color-matrices'
-import { concatTwoColorMatrices, concatColorMatrices } from 'concat-color-matrices'
+import React from "react";
+import { NativeFilter } from "./native-filter";
+import filters from "rn-color-matrices";
+import {
+  concatTwoColorMatrices,
+  concatColorMatrices
+} from "concat-color-matrices";
 
-const ColorMatrixFilter = ({ matrix, parentMatrix, children, ...restProps }) => {
-  const mat = Array.isArray(matrix[0]) ? concatColorMatrices(matrix) : matrix
+const ColorMatrixFilter = ({
+  matrix,
+  parentMatrix,
+  children,
+  ...restProps
+}) => {
+  const mat = Array.isArray(matrix[0]) ? concatColorMatrices(matrix) : matrix;
 
-  const concatedMatrix = parentMatrix ? concatTwoColorMatrices(mat, parentMatrix) : mat
-  const child = React.Children.only(children)
+  const concatedMatrix = parentMatrix
+    ? concatTwoColorMatrices(mat, parentMatrix)
+    : mat;
+  const child = React.Children.only(children);
 
-  return child && child.type && child.type.isColorMatrixFilter
-    ? React.cloneElement(child, { ...child.props, parentMatrix: concatedMatrix })
-    : (
-      <NativeFilter
-        matrix={concatedMatrix}
-        {...restProps}
-      >
-        {children}
-      </NativeFilter>
-    )
-}
+  getImageData = callback => {
+    this.nativeFilter.getImageData(callback);
+  };
 
-ColorMatrixFilter.isColorMatrixFilter = true
-ColorMatrixFilter.displayName = 'ColorMatrix'
+  return child && child.type && child.type.isColorMatrixFilter ? (
+    React.cloneElement(child, { ...child.props, parentMatrix: concatedMatrix })
+  ) : (
+    <NativeFilter
+      ref={e => {
+        this.nativeFilter = e;
+      }}
+      matrix={concatedMatrix}
+      {...restProps}
+    >
+      {children}
+    </NativeFilter>
+  );
+};
 
-const filterName = (name) => {
-  const [first, ...rest] = name.split('')
-  return name === 'rgba' ? 'RGBA' : first.toUpperCase() + rest.join('')
-}
+ColorMatrixFilter.isColorMatrixFilter = true;
+ColorMatrixFilter.displayName = "ColorMatrix";
+
+const filterName = name => {
+  const [first, ...rest] = name.split("");
+  return name === "rgba" ? "RGBA" : first.toUpperCase() + rest.join("");
+};
 
 const filterMap = {
-  ColorTone: (filter) => ({ desaturation, toned, lightColor, darkColor, ...restProps }) => (
+  ColorTone: filter => ({
+    desaturation,
+    toned,
+    lightColor,
+    darkColor,
+    ...restProps
+  }) => (
     <ColorMatrixFilter
       matrix={filter(desaturation, toned, lightColor, darkColor)}
       {...restProps}
     />
   ),
 
-  RGBA: (filter) => ({ red, green, blue, alpha, ...restProps }) => (
+  RGBA: filter => ({ red, green, blue, alpha, ...restProps }) => (
     <ColorMatrixFilter
       matrix={filter(red, green, blue, alpha)}
       {...restProps}
     />
   ),
 
-  DuoTone: (filter) => ({ firstColor, secondColor, ...restProps }) => (
+  DuoTone: filter => ({ firstColor, secondColor, ...restProps }) => (
     <ColorMatrixFilter
       matrix={filter(firstColor, secondColor)}
       {...restProps}
     />
   )
-}
+};
 
-const createFilter = (key) => filterMap[key] || ((filter) => ({ amount, ...restProps }) => (
-  <ColorMatrixFilter
-    matrix={filter(amount)}
-    {...restProps}
-  />
-))
+const createFilter = key =>
+  filterMap[key] ||
+  (filter => ({ amount, ...restProps }) => (
+    <ColorMatrixFilter matrix={filter(amount)} {...restProps} />
+  ));
 
 export default Object.keys(filters).reduce(
   (acc, name) => {
-    const key = filterName(name)
+    const key = filterName(name);
 
-    acc[key] = createFilter(key)(filters[name])
-    acc[key].displayName = key
-    acc[key].isColorMatrixFilter = true
-    return acc
+    acc[key] = createFilter(key)(filters[name]);
+    acc[key].displayName = key;
+    acc[key].isColorMatrixFilter = true;
+    return acc;
   },
   { ColorMatrix: ColorMatrixFilter }
-)
+);
--- src/native-filter.js
+++ src/native-filter.js
@@ -1,16 +1,34 @@
-import React from 'react'
-import { requireNativeComponent } from 'react-native'
-import { defaultStyle, checkStyle } from './style'
+import React from "react";
+import {
+  requireNativeComponent,
+  findNodeHandle,
+  UIManager
+} from "react-native";
+import { defaultStyle, checkStyle } from "./style";
 
-const ColorMatrixImageFilter = requireNativeComponent('CMIFColorMatrixImageFilter')
+const ColorMatrixImageFilter = requireNativeComponent(
+  "CMIFColorMatrixImageFilter"
+);
 
 export const NativeFilter = ({ style, ...restProps }) => {
-  checkStyle(style)
+  checkStyle(style);
+
+  getImageData = callback => {
+    UIManager.dispatchViewManagerCommand(
+      findNodeHandle(this.imageFilter),
+      UIManager.getViewManagerConfig("CMIFColorMatrixImageFilter").Commands
+        .getImageData,
+      [callback]
+    );
+  };
 
   return (
     <ColorMatrixImageFilter
+      ref={e => {
+        this.imageFilter = e;
+      }}
       style={[defaultStyle.container, style]}
       {...restProps}
     />
-  )
-}
+  );
+};
