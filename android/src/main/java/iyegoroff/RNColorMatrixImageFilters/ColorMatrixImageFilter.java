bb2be5bbe20319f175cf121c0636ee6595da8f0d
Steven <trthtai92@gmail.com>
2019-11-08 00:30:32 +0700

    - Convert Bitmap to Base64 string. - Add trigger onDone event for Android.

--- android/src/main/java/iyegoroff/RNColorMatrixImageFilters/ColorMatrixImageFilter.java
+++ android/src/main/java/iyegoroff/RNColorMatrixImageFilters/ColorMatrixImageFilter.java
@@ -1,20 +1,45 @@
 package iyegoroff.RNColorMatrixImageFilters;
 
+import com.facebook.common.executors.CallerThreadExecutor;
+import com.facebook.common.references.CloseableReference;
+import com.facebook.datasource.DataSource;
+import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.generic.RootDrawable;
+import com.facebook.imagepipeline.core.ImagePipeline;
+import com.facebook.imagepipeline.datasource.BaseBitmapDataSubscriber;
+import com.facebook.imagepipeline.image.CloseableImage;
+import com.facebook.imagepipeline.request.ImageRequest;
+import com.facebook.imagepipeline.request.ImageRequestBuilder;
+import com.facebook.react.bridge.Arguments;
+import com.facebook.react.bridge.ReactContext;
+import com.facebook.react.bridge.WritableMap;
+import com.facebook.react.uimanager.events.RCTEventEmitter;
 import com.facebook.react.views.view.ReactViewGroup;
 
 import android.content.Context;
+import android.graphics.Bitmap;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.drawable.BitmapDrawable;
+import android.net.Uri;
+import android.util.Base64;
+import android.util.Log;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 
+import androidx.annotation.Nullable;
+
 import com.facebook.react.bridge.ReadableArray;
+
+import java.io.ByteArrayOutputStream;
 
 public class ColorMatrixImageFilter extends ReactViewGroup {
 
   private ColorMatrixColorFilter mFilter = new ColorMatrixColorFilter(new ColorMatrix());
+  private String uri = new String();
 
   public ColorMatrixImageFilter(Context context) {
     super(context);
@@ -32,8 +57,8 @@ public class ColorMatrixImageFilter extends ReactViewGroup {
     invalidate();
   }
 
-  public void onReceiveNativeEvent() {
-    
+  public void setUri(String uri) {
+    this.uri = uri;
   }
 
   @Override
@@ -47,22 +72,52 @@ public class ColorMatrixImageFilter extends ReactViewGroup {
 
       if (child instanceof ImageView) {
         ((ImageView) child).setColorFilter(mFilter);
-        ImageView imageView = (ImageView)child;
-        imageView.buildDrawingCache();
-        BitmapDrawable drawable = (BitmapDrawable) imageView.getDrawable();
-        Bitmap bitmap = drawable.getBitmap();
-        ByteArrayOutputStream bos = new ByteArrayOutputStream();  
-        bmap.compress(CompressFormat.PNG, 100, bos); 
-        byte[] bb = bos.toByteArray();
 
-        WritableMap event = Arguments.createMap();
-        event.putString("data", Base64.encodeBytes(bb)); 
-        ReactContext reactContext = (ReactContext)getContext();
-        reactContext.getJSModule(RCTEventEmitter.class).receiveEvent(
-            getId(),
-            "onDone",
-            event);
-        }
+        ImageRequest imageRequest = ImageRequestBuilder
+                .newBuilderWithSource(Uri.parse(this.uri))
+                .setProgressiveRenderingEnabled(true)
+                .build();
+
+        ImagePipeline imagePipeline = Fresco.getImagePipeline();
+        final DataSource<CloseableReference<CloseableImage>>
+                dataSource = imagePipeline.fetchDecodedImage(imageRequest, this);
+
+        dataSource.subscribe(new BaseBitmapDataSubscriber() {
+
+          @Override
+          public void onNewResultImpl(@Nullable Bitmap bitmap) {
+            if (dataSource.isFinished() && bitmap != null){
+              Bitmap bmp = Bitmap.createBitmap(bitmap);
+              Paint paint = new Paint();
+              paint.setColorFilter(mFilter);
+
+              Canvas canvas = new Canvas(bmp);
+              canvas.drawBitmap(bmp, 0, 0, paint);
+
+              ByteArrayOutputStream bos = new ByteArrayOutputStream();
+              bmp.compress(Bitmap.CompressFormat.PNG, 100, bos);
+              byte[] bb = bos.toByteArray();
+              String encodedString = Base64.encodeToString(bb, 0);
+
+              WritableMap event = Arguments.createMap();
+              event.putString("data", encodedString);
+              ReactContext reactContext = (ReactContext)getContext();
+              reactContext.getJSModule(RCTEventEmitter.class).receiveEvent(
+                      getId(),
+                      "onDone",
+                      event);
+
+              dataSource.close();
+            }
+          }
+
+          @Override
+          public void onFailureImpl(DataSource dataSource) {
+            if (dataSource != null) {
+              dataSource.close();
+            }
+          }
+        }, CallerThreadExecutor.getInstance());
 
         break;
       }
--- android/src/main/java/iyegoroff/RNColorMatrixImageFilters/ColorMatrixImageFilterManager.java
+++ android/src/main/java/iyegoroff/RNColorMatrixImageFilters/ColorMatrixImageFilterManager.java
@@ -1,10 +1,13 @@
 package iyegoroff.RNColorMatrixImageFilters;
 
 import com.facebook.react.bridge.ReadableArray;
+import com.facebook.react.common.MapBuilder;
 import com.facebook.react.module.annotations.ReactModule;
 import com.facebook.react.uimanager.annotations.ReactProp;
 import com.facebook.react.views.view.ReactViewManager;
 import com.facebook.react.uimanager.ThemedReactContext;
+
+import java.util.Map;
 
 @ReactModule(name = ColorMatrixImageFilterManager.REACT_CLASS)
 public class ColorMatrixImageFilterManager extends ReactViewManager {
@@ -12,6 +15,7 @@ public class ColorMatrixImageFilterManager extends ReactViewManager {
   static final String REACT_CLASS = "CMIFColorMatrixImageFilter";
 
   private static final String PROP_MATRIX = "matrix";
+  private static final String PROP_URI = "uri";
 
   @Override
   public String getName() {
@@ -37,5 +41,10 @@ public class ColorMatrixImageFilterManager extends ReactViewManager {
   @ReactProp(name = PROP_MATRIX)
   public void setMatrix(ColorMatrixImageFilter view, ReadableArray matrix) {
     view.setMatrix(matrix);
+  }
+
+  @ReactProp(name = PROP_URI)
+  public void setMatrix(ColorMatrixImageFilter view, String uri) {
+    view.setUri(uri);
   }
 }
